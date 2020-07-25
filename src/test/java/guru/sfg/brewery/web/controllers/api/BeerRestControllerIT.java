2363f9ded2dbae5615af8188b30c1bc441b5af03
Yatin <yatin.batra@nagarro.com>
2020-07-25 17:25:07 +0530

    get-beer-permit-all: adding the new test to check if the post request says authenticated

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -23,5 +24,10 @@ public class BeerRestControllerIT extends BaseIT {
     void findBeerById() throws Exception{
         mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws  Exception {
+        mockMvc.perform(post("/api/v1/beer/")).andExpect(status().isForbidden());
     }
 }
